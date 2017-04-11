8da4cf696d9cafde8006a8b3d51b787de6195de2
Travis Gorman <thefarbetween@me.com>
2017-04-11 12:43:38 -0500

    first component of svideo component

--- src/App.js
+++ src/App.js
@@ -1,60 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
+const App = React.createClass({
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img 
-            src={logo} 
-            className="App-logo" 
-            alt="logo" />
-          <h2>Welcome to React</h2>
+    return (  
+      <div className="app">
+        <div className="landing">
+          <h1>svideo</h1>
+          <input type="text" placeholder="Search"/>
+          <a href="#">or Browse All</a>
         </div>
-        <MyFirstComponent />
       </div>
-    );
-  }
-}
-
-var div = React.DOM.div
-var h1 = React.DOM.h1
-
-const MyTitle = React.createClass({
-  render() {
-    return (
-      div(null, 
-        h1({style: {color:this.props.color}}, 
-          this.props.title)
-      )
     )
   }
-});
-
-var MyTitleFactory = React.createFactory(MyTitle)
-
-var MyFirstComponent = React.createClass({
-  render: function() {
-    return (
-      div(null, 
-        MyTitleFactory({
-            title: 'props are the best', color: 'peru'
-        }),
-        MyTitleFactory({
-            title: 'semicolons are the worst', color: 'mediumaquamarine',
-        }),
-        MyTitleFactory({
-            title: 'jk its okay if you like semicolons', color: 'papayawhip',
-        }),
-        MyTitleFactory({
-            title: 'nah, just kidding. fuck semicolons', color: 'palevioletred',
-        })
-      )
-    )
-  }
-});
-
+})
 
 export default App;
