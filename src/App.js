3dc6948f5a1a7a52be25b637393f67fd0450c49c
Travis Gorman <thefarbetween@me.com>
2017-04-12 10:18:48 -0500

    map inside search component returning showcard in markup

--- src/App.js
+++ src/App.js
@@ -35,11 +35,18 @@ const Search = React.createClass({
   render () {
     return (
       <div className="search">
-        <pre>
-          <code>
-            {JSON.stringify(preload, null, 4)}
-          </code>
-        </pre>
+        {preload.shows.map((show) => {
+          return (
+            <div className="show-card">
+              <img src={`/public/img/posters/${show.poster}`} />
+              <div>
+                <h3>{show.title}</h3>
+                <h4>({show.year})</h4>
+                <p>{show.description}</p>
+              </div>
+            </div>
+          )
+        })}
       </div>
     )
   }
