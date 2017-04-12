3c5428b980ccc8ca5cded6d40eefb35d335ee096
Travis Gorman <thefarbetween@me.com>
2017-04-12 13:28:01 -0500

    added search functionality

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ const Landing = React.createClass({
 const Search = React.createClass({
   getInitialState () {
     return {
-      searchTerm: 'this is the default string'
+      searchTerm: ''
     }
   },
   handleSearchTermChange (event) {
@@ -46,7 +46,7 @@ const Search = React.createClass({
     return (
       <div className='search'>
         <header>
-          <h1>{this.state.searchTerm}</h1>
+          <h1>Squirk Movies!</h1>
           <input
             type="text"
             placeholder="Search..."
@@ -54,13 +54,20 @@ const Search = React.createClass({
             onChange={this.handleSearchTermChange} />
         </header>
         <div>
-        {
-          preload.shows.map((show) => {
-            return (
-              <ShowCard key={show.imdbID} {...show} />
-            )
-          })
-        }
+          { preload.shows
+              .filter((show) => {
+                return `${show.title} ${show.description}`
+                  .toUpperCase()
+                    .indexOf(
+                      this.state.searchTerm
+                        .toUpperCase()) >= 0
+              })
+              .map((show) => {
+                return (
+                  <ShowCard key={show.imdbID} {...show} />
+                )
+              })
+          }
         </div>
       </div>
     )
