15ab4e703cbe4f3e2824ca670abbdb86404f51f9
Travis Gorman <thefarbetween@me.com>
2017-04-11 23:11:58 -0500

    Landing component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import './App.css';
 
 const App = React.createClass({
   render() {
-    return (  
+    return (
       <div className="app">
-        <div className="landing">
-          <h1>svideo</h1>
-          <input type="text" placeholder="Search"/>
-          <a href="#">or Browse All</a>
-        </div>
+        <Landing />
+      </div>
+    )
+  }
+})
+
+const Landing = React.createClass({
+  render () {
+    return (
+      <div className="landing">
+        <h1>patterns</h1>
+        <input type="text" placeholder="Search"/>
+        <a href="">or Browse All</a>
       </div>
     )
   }
