80efa748bd2fde2b95ec0a0ce602d3ab56931308
Travis Gorman <thefarbetween@me.com>
2017-04-16 00:43:10 -0500

    preload shows

--- src/Search.js
+++ src/Search.js
@@ -25,20 +25,12 @@ const Search = React.createClass({
             onChange={this.handleSearchTermChange} />
         </header>
         <div>
-          { preload.shows
-              .filter((show) => {
-                return `${show.title} ${show.description}`
-                  .toUpperCase()
-                    .indexOf(
-                      this.state.searchTerm
-                        .toUpperCase()) >= 0
-              })
-              .map((show) => {
-                return (
-                  <ShowCard key={show.imdbID} {...show} />
-                )
-              })
-          }
+          {preload.shows
+            .filter((show) =>
+              `${show.title} ${show.description}`.toUpperCase()
+                .indexOf(this.state.searchTerm.toUpperCase()) >= 0 )
+                  .map((show) =>
+                      <ShowCard key={show.imdbID} {...show} />)}
         </div>
       </div>
     )
