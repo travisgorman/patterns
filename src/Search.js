2b62ab74051e2ae15d853ede3f22eb53325b3bf9
Travis Gorman <thefarbetween@me.com>
2017-04-16 08:44:10 -0500

    pass props to Search through router instead of importing directly

--- src/Search.js
+++ src/Search.js
@@ -3,38 +3,38 @@ import preload from './data.json'
 import ShowCard from './ShowCard'
 
 const Search = React.createClass({
+  propTypes: {
+    shows: arrayOf(shape({
+      title: string,
+      description: string
+    }))
+  },
   getInitialState () {
     return {
       searchTerm: ''
     }
   },
   handleSearchTermChange (event) {
-    this.setState({
-      searchTerm: event.target.value
-    })
+    this.setState({searchTerm: event.target.value})
   },
   render () {
     return (
       <div className='search'>
         <header>
-          <h1>Squirk Movies!</h1>
-          <input
-            type="text"
-            placeholder="Search..."
-            value={this.state.searchTerm}
-            onChange={this.handleSearchTermChange} />
+          <h1>svideo</h1>
+          <input onChange={this.handleSearchTermChange} value={this.state.searchTerm} type='text' placeholder='Search' />
         </header>
         <div>
-          {preload.shows
-            .filter((show) =>
-              `${show.title} ${show.description}`.toUpperCase()
-                .indexOf(this.state.searchTerm.toUpperCase()) >= 0 )
-                  .map((show) =>
-                      <ShowCard key={show.imdbID} {...show} />)}
+          {this.props.shows
+            .filter((show) => `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0)
+            .map((show) => {
+              return (
+                <ShowCard key={show.imdbID} {...show} />
+              )
+            })}
         </div>
       </div>
     )
   }
 })
-
 export default Search
