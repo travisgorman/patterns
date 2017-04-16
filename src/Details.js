2e59ecef1da49d18e51374dbba4fe2ac86aa0ede
Travis Gorman <thefarbetween@me.com>
2017-04-16 08:28:07 -0500

    details- stateless functional component

--- src/Details.js
+++ src/Details.js
@@ -1,23 +1,23 @@
 import React from 'react'
 
-const Details = React.createClass({
-  render () {
-    return (
-      <div className='details'>
-        <pre>
-          <code>
-            {JSON.stringify(this.props, null, 4)}
-          </code>
-        </pre>
-      </div>
-    )
-  }
-})
+// const Details = React.createClass({
+//   render () {
+//     return (
+//       <div className='details'>
+//         <pre>
+//           <code>
+//             {JSON.stringify(this.props, null, 4)}
+//           </code>
+//         </pre>
+//       </div>
+//     )
+//   }
+// })
 
 // stateless functional component
 
-// const Details = (props) => {
-//   return <h1>{props.params.id}</h1>
-// }
+const Details = (props) => {
+  return <h1>{props.params.id}</h1>
+}
 
 export default Details
