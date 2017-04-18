7de3309a484fc78ab3478cceb0cc7db34c10a250
Travis Gorman <thefarbetween@me.com>
2017-04-18 10:46:47 -0500

    setSearchTerm reducer

--- src/reducers.js
+++ src/reducers.js
@@ -2,6 +2,14 @@ const DEFAULT_STATE = {
   searchTerm: ''
 }
 
+const setSearchTerm = (state, action) => {
+  const newState = {}
+  Object.assign(newState, state, {
+    searchTerm: action.searchTerm
+  })
+  return newState
+}
+
 const rootReducer = (state = DEFAULT_STATE, action) => {
   // usually a big switch statement
   // not using combineReducer
