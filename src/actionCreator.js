4c6177e2fdc59cb21f81711b1e986b0d311465f9
Travis Gorman <thefarbetween@me.com>
2017-04-22 15:49:29 -0500

    addOMDBDetails action creator - thunk: returns a function

--- src/actionCreator.js
+++ src/actionCreator.js
@@ -1,4 +1,5 @@
 import { SET_SEARCH_TERM, ADD_OMDB_DATA } from './actions'
+import axios from 'axios'
 
 export function setSearchTerm (searchTerm) {
   return { type: SET_SEARCH_TERM, searchTerm }
@@ -6,4 +7,14 @@ export function setSearchTerm (searchTerm) {
 
 export function addOMDBData (imdbID, omdbData) {
   return { type: ADD_OMDB_DATA, imdbID, omdbData }
+}
+
+export function addOMDBDetails (imdbID) {
+  return function (dispatch, getState) {
+    axios.get(`http://www.omdbid.com/?i=${imdbID}`)
+    .then((response) => {
+      dispatch({ imdbID, omdbData: response.data })
+    })
+    .catch((error) => console.error('axios error', error))
+  }
 }
