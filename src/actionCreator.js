b05385c4219916c25452cf62692544aebc67c7c5
Travis Gorman <thefarbetween@me.com>
2017-04-22 15:32:49 -0500

    addOMDBData action creator

--- src/actionCreator.js
+++ src/actionCreator.js
@@ -1,5 +1,9 @@
-import { SET_SEARCH_TERM } from './actions'
+import { SET_SEARCH_TERM, ADD_OMDB_DATA } from './actions'
 
 export function setSearchTerm (searchTerm) {
   return { type: SET_SEARCH_TERM, searchTerm }
+}
+
+export function addOMDBData (imdbID, omdbData) {
+  return { type: ADD_OMDB_DATA, imdbID, omdbData }
 }
