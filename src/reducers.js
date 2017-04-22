fbb43c51583c190fedcf5dcbff762f067247c5f8
Travis Gorman <thefarbetween@me.com>
2017-04-22 15:30:03 -0500

    addOMDBData reducer

--- src/reducers.js
+++ src/reducers.js
@@ -1,17 +1,30 @@
-import { SET_SEARCH_TERM } from './actions'
+import { SET_SEARCH_TERM, ADD_OMDB_DATA } from './actions'
 
-const DEFAULT_STATE = { searchTerm: '' }
+const DEFAULT_STATE = {
+  searchTerm: '',
+  omdbData: {}
+}
 
 const setSearchTerm = (state, action) => {
   const newState = {}
   Object.assign(newState, state, { searchTerm: action.searchTerm })
-  return newstate
+  return newState
+}
+
+const addOMDBData (state, action) => {
+  const newOMDBData = {}
+  Object.assign(newOMDBData, state.omdbData, { [action.imdbID] : action.omdbData })
+  const newState = {}
+  Object.assign(newState, state, { omdbData: newOMDBData })
+  return newState
 }
 
 const rootReducer = (state = DEFAULT_STATE, action) => {
   switch (action.type) {
     case SET_SEARCH_TERM:
       return setSearchTerm(state, action)
+    case ADD_OMDB_DATA:
+      return addOMDBData(state, action)
     default:
       return state
   }
